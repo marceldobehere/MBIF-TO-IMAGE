38b124d7b3bad3a8dbb226ed53283f7c527b6cda
Marceldobehere <marcel.z2006@gmail.com>
2022-06-17 10:44:44 +0200

    Projektdateien hinzufügen.

---
+++ MBIF TO IMAGE/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace MBIF_TO_IMAGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No File provided!");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine();
+
+            if (Directory.Exists(args[0]))
+            {
+                string newFolder = $"{args[0]}-deconv";
+                Directory.CreateDirectory(newFolder);
+                string[] files = Directory.GetFiles(args[0]);
+                foreach (string file in files)
+                    DeconvImage(file, newFolder);
+            }
+            else if (File.Exists(args[0]))
+            {
+                DeconvImage(args[0], ".");
+            }
+            else
+            {
+                Console.WriteLine("No valid File provided!");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("\n\nEnd.");
+            Console.ReadLine();
+        }
+
+        static void DeconvImage(string path, string resPath)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+
+
+            //    writer.Write(image.Width);          // 4 byte width
+            //    writer.Write(image.Height);         // 4 byte height
+            //    writer.Write(xOff);                 // 4 byte x offset
+            //    writer.Write(yOff);                 // 4 byte y offset
+            //    writer.Write(imageData.LongLength); // 8 byte image lenght
+            //    writer.Write(imageData);            // image data
+
+            int width = BitConverter.ToInt32(bytes, 0);
+            int height = BitConverter.ToInt32(bytes, 4);
+            int xOff = BitConverter.ToInt32(bytes, 8);
+            int yOff = BitConverter.ToInt32(bytes, 12);
+            long dataLen = BitConverter.ToInt64(bytes, 16);
+            int offset = 4+4+4+4+8;
+
+            Console.WriteLine($"Image:");
+            Console.WriteLine($" - Size:   {width}x{height}");
+            Console.WriteLine($" - Offset: ({xOff}, {yOff})");
+            Console.WriteLine($" - Data Lenght: {dataLen}");
+            Console.WriteLine();
+            Bitmap image = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+
+
+            BitmapData bitmapData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            Console.WriteLine($"Image: {image.Width}x{image.Height} = {image.Width * image.Height} ({image.Width * image.Height * 4} bytes)");
+            byte[] imageData = new byte[image.Height * image.Width * 4];
+            try
+            {
+                unsafe
+                {
+                    byte* ptr = (byte*)bitmapData.Scan0;
+                    for (int i = 0; i < image.Height * image.Width * 4; i++)
+                        ptr[i] = bytes[i+offset];
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Reading Image failed!");
+            }
+            Console.WriteLine("Image was read.");
+            Console.WriteLine();
+            image.Save($"{resPath}/{Path.GetFileNameWithoutExtension(path)}.bmp");
+
+
+            //Console.WriteLine("Writing Image File.");
+            //using (BinaryWriter writer = new BinaryWriter(new FileStream($"{resPath}/{Path.GetFileNameWithoutExtension(path)}.mbif", FileMode.Create)))
+            //{
+            //    writer.Write(image.Width);          // 4 byte width
+            //    writer.Write(image.Height);         // 4 byte height
+            //    writer.Write(xOff);                 // 4 byte x offset
+            //    writer.Write(yOff);                 // 4 byte y offset
+            //    writer.Write(imageData.LongLength); // 8 byte image lenght
+            //    writer.Write(imageData);            // image data
+            //}
+            //Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ MBIF TO IMAGE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("MBIF TO IMAGE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MBIF TO IMAGE")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("3d0615d6-799b-444f-b028-48b743f3a55b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
